3e0998ef3f3b2e96c3e2e53459c0de2a0e05b27e
Kutman <muctep.k@gmail.com>
2019-10-01 19:39:25 +0600

    Модуль views.py превращен в пакет views, разделены представления по файлам.

---
+++ source/webapp/views/__init__.py
@@ -0,0 +1,4 @@
+from .article_views import IndexView, ArticleCreateView, ArticleUpdateView, \
+    ArticleDeleteView, ArticleView
+from .comment_views import CommentView, CommentCreateInArticleView, CommentCreateView, \
+    CommentDeleteView, CommentUpdateView
---
+++ source/webapp/views/article_views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.forms import ArticleForm, CommentInArticleForm
+from webapp.models import Article, Comment
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'article/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
+
+
+class ArticleView(TemplateView):
+    template_name = 'article/article.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        context = super().get_context_data(**kwargs)
+        context['article'] = get_object_or_404(Article, pk=pk)
+        context['form'] = CommentInArticleForm()
+        context['comments'] = Comment.objects.all().filter(article_id=pk).order_by('-created_at')
+        return context
+
+
+class ArticleCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'article/create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = Article.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text']
+            )
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'article/create.html', context={'form': form})
+
+
+class ArticleUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs['pk'])
+        form = ArticleForm(data={
+            'title': article.title,
+            'text': article.text,
+            'author': article.author
+        })
+        return render(request, 'article/update.html', context={'form': form, 'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs['pk'])
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article.title = form.cleaned_data['title']
+            article.text = form.cleaned_data['text']
+            article.author = form.cleaned_data['author']
+            article.save()
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'article/update.html', context={'form': form, 'article': article})
+
+
+class ArticleDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs['pk'])
+        return render(request, 'article/delete.html', context={'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs['pk'])
+        article.delete()
+        return redirect('index')
